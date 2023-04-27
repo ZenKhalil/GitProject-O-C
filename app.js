fab035ac653e36d614e42b05bdae796544737ce0
svdf18 <simonvandeurs@hotmail.com>
2023-04-27 09:28:58 +0200

    helloWorld function add

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ window.addEventListener("load", initApp);
 function initApp() {
 console.log("app.js code is working!");
 const string = "Hello world";
+helloWorld()
 }
 
+function helloWorld(){
+    console.log("hello Rolf & Mohammed")
+}
