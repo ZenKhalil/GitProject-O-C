970bb4dbd731fee244a601bb058294399a8df1f7
Rolf <rarogbennu@gmail.com>
2023-04-27 09:29:17 +0200

    new function

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ window.addEventListener("load", initApp);
 function initApp() {
 console.log("app.js code is working!");
 const string = "Hello world";
+showText(string)
 }
 
+function showText(string) {
+    console.log(string);
+}
